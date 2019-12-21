4ed8a165e6f076443956ba494d12b85ae1125213
akshayraj13 <akshyrraj@gmail.com>
2019-12-22 00:50:21 +0530

    adding save, edit and add functionality

--- notes-app/src/app/app.component.ts
+++ notes-app/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Note} from './models/note.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'notes-app';
+export class AppComponent implements OnInit{
+  notes: Note[];
+  selectedNote: Note;
+  ngOnInit(): void {
+    this.getNotes();
+  }
+
+  private getNotes(){
+
+    if(localStorage.getItem('notes') == null){
+      this.notes =  new Array<Note>();
+    } else {
+      this.notes = JSON.parse(localStorage.getItem('notes'));
+    }
+  }
+  public addNote() {
+    this.selectedNote = new Note();
+    this.notes.push(this.selectedNote);
+  }
+
+  public saveNotes(){
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+
+  editNote(note: Note){
+    this.selectedNote = note;
+  }
 }
